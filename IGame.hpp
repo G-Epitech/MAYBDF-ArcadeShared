7e05c8a2d70c5447dacb52cd7ca4123a8f28a216
Matheo Coquet <matheo.coquet@epitech.eu>
2024-03-11 18:21:27 +0100

    feat: add project template

---
+++ IGame.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** shared-arcade
+** File description:
+** IGame
+*/
+
+#pragma once
+
+class IGame {
+  public:
+    IGame();
+    ~IGame();
+
+  protected:
+  private:
+};
