4fc7e6d569395ed6d629169e4cfd51c6e264ae51
Flavien Chenu <flavienchenu@hotmail.com>
2024-03-16 13:07:18 +0100

    feat(types): add UUID type

---
+++ types/UUID.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** arcade-shared
+** File description:
+** UUID
+*/
+
+#pragma once
+
+namespace shared {
+    namespace types {
+        typedef unsigned long long UUID;
+    }
+}
