aed77e3aded5c7c9d4fda6d9a3ecffd0e74367a4
Matheo Coquet <matheo.coquet@epitech.eu>
2024-03-16 12:51:51 +0100

    feat(types): add vector type

---
+++ types/Vector.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** shared-arcade
+** File description:
+** Vector
+*/
+
+#pragma once
+
+namespace shared
+{
+  namespace types
+  {
+    template <typename T>
+    struct Vector;
+
+    typedef struct Vector<int> Vector2i;
+    typedef struct Vector<float> Vector2f;
+    typedef struct Vector<unsigned int> Vector2u;
+  }
+}
+
+template <typename T>
+struct shared::types::Vector
+{
+  Vector(T x, T y) : x(x), y(y){};
+
+  T x;
+  T y;
+};
