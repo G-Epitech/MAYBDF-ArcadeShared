755d0df75692f368d8030af790539bf1469f15b6
sdragos1 <dragossuceveanu@yahoo.com>
2024-03-27 14:54:26 +0100

    fix: color type syntax

--- types/Color.hpp
+++ types/Color.hpp
@@ -9,9 +9,10 @@
 
 namespace shared::types
 {
-    typedef Primary unsigned char;
-    typedef struct {
-        Color(Primary r, Primary g, Primary b, Primary a) : r(r), g(g), b(b), a(a){};
+    typedef unsigned char Primary;
+
+    typedef struct Color_s {
+        Color_s(Primary r, Primary g, Primary b, Primary a) : r(r), g(g), b(b), a(a) {}
 
         Primary r;
         Primary g;
