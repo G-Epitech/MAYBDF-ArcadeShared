8f3effb52a9b2f5406a7562113fc74eae3f51cfc
Matheo Coquet <matheo.coquet@epitech.eu>
2024-04-02 11:22:10 +0200

    fix: remove useless reference of shared ptr for game events

--- games/components/IDisplayableComponent.hpp
+++ games/components/IDisplayableComponent.hpp
@@ -29,17 +29,17 @@ public:
      * @brief On click event handler for the entity
      * @param ctx Context of the game
      */
-    virtual void onMousePress(std::shared_ptr <IGame> &ctx) = 0;
+    virtual void onMousePress(std::shared_ptr <IGame> ctx) = 0;
 
     /**
      * @brief On release event handler for the entity
      * @param ctx Context of the game
      */
-    virtual void onMouseRelease(std::shared_ptr <IGame> &ctx) = 0;
+    virtual void onMouseRelease(std::shared_ptr <IGame> ctx) = 0;
 
     /**
      * @brief On hover event handler for the entity
      * @param ctx Context of the game
      */
-    virtual void onMouseHover(std::shared_ptr <IGame> &ctx) = 0;
+    virtual void onMouseHover(std::shared_ptr <IGame> ctx) = 0;
 };
--- games/components/IPositionableComponent.hpp
+++ games/components/IPositionableComponent.hpp
@@ -23,11 +23,11 @@ class shared::games::components::IPositionableComponent: public virtual ICompone
      * @brief Get position of the entity (tiles)
      *
      */
-    virtual types::Vector2i &getPosition(void) noexcept = 0;
+    virtual types::Vector2i &getPosition() noexcept = 0;
 
     /**
      * @brief Get size of the entity (tiles)
      *
      */
-    virtual types::Vector2u &getSize(void) noexcept = 0;
+    virtual types::Vector2u &getSize() noexcept = 0;
 };
